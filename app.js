fe8d0de14745b15328f056cac38bd3bb7d5132a1
HarshithReddy11 <sharshithreddy23@ifheindia.org>
2026-06-11 11:47:31 +0530

    chatbot

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import "@/App.css";
+import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import axios from "axios";
 import { HOME } from "@/constants/testIds";
